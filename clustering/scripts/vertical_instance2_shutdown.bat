8363698b9e5dd8a74192f7bf380774b24d99e8f4
Liam, Song <blackdogcode@gmail.com>
2020-02-19 16:08:05 +0900

    Tomcat Vertical Clustering Instances

---
+++ clustering/scripts/vertical_instance2_shutdown.bat
@@ -0,0 +1,5 @@
+set CATALINA_HOME=C:\Program Files\Apache Software Foundation\Tomcat 9.0
+set CATALINA_BASE=C:\Users\dilet\Desktop\GitHub\ApacheTomcat-Bootstrap\clustering\vertical_instance_2
+
+cd %CATALINA_HOME%\bin
+shutdown.bat
